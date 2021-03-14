54c9189e00dbee926e055c3ad6ce7386fbb17ef7
ParitoshBh <mail@paritoshbh.me>
2021-03-14 18:12:38 +0000

    Load connection config from json file

--- backend/connections/s3.go
+++ backend/connections/s3.go
@@ -1,33 +1,50 @@
 package connections
 
 import (
-	"context"
+	"go-organizer/backend/logger"
+	"io/ioutil"
 
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/tidwall/gjson"
 )
 
-var (
-	MinioClient *minio.Client
-	Context     context.Context
-)
+var s3Client *s3.S3
 
-// GetS3Connection builds and return connection to S3
-func GetS3Connection() error {
-	endpoint := ""
-	accessKeyID := ""
-	secretAccessKey := ""
-	useSSL := false
+// BuildS3Connection builds and return connection to S3
+func BuildS3Connection() {
+	_logger := logger.Logger
 
-	// Initialize minio client object.
-	minioClient, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-		Secure: useSSL,
+	// load file
+	file, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		_logger.Error(err.Error())
+	}
+
+	// validate file as json
+	if !gjson.Valid(string(file)) {
+		_logger.Fatal("Invalid config file")
+	}
+
+	s3Config := gjson.Parse(string(file)).Get("connections.s3")
+
+	// Initialize s3 client
+	newSession, err := session.NewSession(&aws.Config{
+		Credentials:      credentials.NewStaticCredentials(s3Config.Get("accessKey").String(), s3Config.Get("secret").String(), ""),
+		Endpoint:         aws.String(s3Config.Get("endpoint").String()),
+		Region:           aws.String("us-east-1"),
+		DisableSSL:       aws.Bool(s3Config.Get("disableSSL").Bool()),
+		S3ForcePathStyle: aws.Bool(true),
 	})
 	if err != nil {
-		return err
+		_logger.Fatal(err.Error())
 	}
-	MinioClient = minioClient
 
-	return nil
+	s3Client = s3.New(newSession)
+}
+
+func GetS3Client() *s3.S3 {
+	return s3Client
 }
--- go.mod
+++ go.mod
@@ -6,13 +6,19 @@ require (
 	github.com/alexedwards/scs/sqlite3store v0.0.0-20210131110750-3ca38f9a41cc
 	github.com/alexedwards/scs/v2 v2.4.0
 	github.com/aws/aws-sdk-go v1.37.26
+	github.com/cosmtrek/air v1.21.2 // indirect
+	github.com/creack/pty v1.1.11 // indirect
 	github.com/dustin/go-humanize v1.0.0
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/mattn/go-sqlite3 v1.14.6
 	github.com/minio/minio-go/v7 v7.0.8
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/tidwall/gjson v1.6.8
 	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/sys v0.0.0-20210313202042-bd2e13477e9c // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.20.12
